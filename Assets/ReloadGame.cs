7cf23fbf373f669d87f19896d5527c0fe3736c3b
yodan <yodanbrandon@gmail.com>
2025-06-03 13:22:37 +0200

    scenemanage

--- Assets/ReloadGame.cs
+++ Assets/ReloadGame.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class ReloadGame : MonoBehaviour
 {
     [SerializeField] private Défilement value;
     [SerializeField] private float valueBaseSpeed;
-    public void Reload(InputAction.CallbackContext context)
+    public void Reload()
     {
-        if (context.performed)
-        {
             value.timeToMove = valueBaseSpeed;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
-        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
